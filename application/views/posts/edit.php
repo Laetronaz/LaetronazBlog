21f50a3d03b86b576ef1dd8d3820c27260ad56f6
Laetronaz <michel.pelland12@gmail.com>
2018-07-13 17:31:17 -0400

    -Version: 0.3.0 -Added Categories to the posts -Added an image the posts -fixed the css file link

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -12,5 +12,13 @@
     <label for="exampleFormControlSelect1">Body</label>
     <textarea id="editor1" class="form-control" name="body" placeholder="Add body"><?php echo $post['body']; ?></textarea>
   </div>
+  <div class = "form-group">
+    <label>Category</label>
+    <select name="category_id" class="form-control">
+      <?php foreach($categories as $category): ?>
+        <option <?php if ($post['category_id'] == $category['id']){ echo "selected"; }?> value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+      <?php endforeach; ?>
+    </select>
+  </div>
   <button type="submit" class="btn btn-default">Submit</button>
 </form>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
 <head>
     <title>Laetronaz Blog</title>
     <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     <script src="http://cdn.ckeditor.com/4.10.0/standard/ckeditor.js"></script>
 </head>
 <body>
