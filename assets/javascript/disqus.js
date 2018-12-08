838e268d1549ee27af56a175ff147626d60fb40f
Michel Pelland <michel.pelland12@gmail.com>
2018-12-08 14:11:52 -0500

    Removed forgotten line in disqus.js
    Removed forgotten line in disqus.js

--- assets/javascript/disqus.js
+++ assets/javascript/disqus.js
@@ -8,4 +8,3 @@
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
 })();
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
