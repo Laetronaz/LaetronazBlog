1f9bb650d2622219ca9aa5e179252914c8a6a9ab
Michel Pelland <michel.pelland12@gmail.com>
2018-12-08 14:07:30 -0500

    Change disqus site for the heroku app
    changed the disqus site for the heroku app

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -25,11 +25,6 @@
         this.page.url = '<?php echo current_url(); ?>'; // Replace PAGE_URL with your page's canonical URL variable
         this.page.identifier = '<?php echo $post['slug']; ?>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
-    (function() { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://laetronazblog.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<?= vdebug(current_url());?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -19,6 +19,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- LOCAL SCRIPTS -->
     <script src="<?php echo base_url(); ?>assets/javascript/tagsinput.js"></script>
+    <script src="<?php echo base_url(); ?>assets/javascript/disqus.js"></script>
   </head>
 <body>
 
