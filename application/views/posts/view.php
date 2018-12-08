a0a61fd52f0388d7225080f8599399ac52f8e112
Michel Pelland <michel.pelland12@gmail.com>
2018-12-08 14:09:39 -0500

    Remove debug
    Removed debug

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -27,4 +27,3 @@
     };
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-<?= vdebug(current_url());?>
