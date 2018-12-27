927380f2c9899a10fb65f5f41613caf85d6c477b
Michel Pelland <michel.pelland12@gmail.com>
2018-12-27 15:33:34 -0500

    Attempts to fix library loading
    Changed class name From Access_Control to Access_control

--- application/libraries/Access_Control.php
+++ application/libraries/Access_Control.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Access_Control {
+class Access_control {
         private const IMAGE_PATH = 'logged_in';
         
         
